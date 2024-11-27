09bf66dd4466be725675f793931845198f779ea6
ROMUSKING <romusking@gmail.com>
2024-11-27 15:18:37 +0000

    added admin endpoints and a bunch of logic probably

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
@@ -15,13 +17,53 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+	apiCfg := apiConfig{}
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
-
 	})
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("public"))))
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.middlewareMetricsDsp)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.middlewareMetricsRst)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix(
+			"/app", http.FileServer(
+				http.Dir("public")))))
+
 	log.Fatal(s.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsDsp(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
+
+}
+
+func (cfg *apiConfig) middlewareMetricsRst(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Counter reset to zero"))
+
 }
