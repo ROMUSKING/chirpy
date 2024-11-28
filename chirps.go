5a0372d391bbf2544edf263f2afa95b3bbeb96f3
ROMUSKING <romusking@gmail.com>
2024-11-28 16:47:36 +0000

    added endpoint for adding chirps

--- chirps.go
+++ chirps.go
@@ -7,14 +7,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/romusking/chirpy/internal/database"
 )
 
 type Chirp struct {
-	ID        uuid.UUID  `json:"id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	Body      string     `json:"body"`
-	UserID    *uuid.UUID `json:"user_id"`
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func (cfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
@@ -37,34 +38,22 @@ func (cfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
 	}
 	params.Body = filterProfane(params.Body)
 
-	userDB, err := cfg.db.CreateUser(r.Context(), params.Email)
+	chirpDB, err := cfg.db.CreateChirp(
+		r.Context(), database.CreateChirpParams{
+			Body:   params.Body,
+			UserID: params.UserID})
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't create user, database error.", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp, database error.", err)
 		return
 	}
-	user := User{
-		ID:        userDB.ID,
-		CreatedAt: userDB.CreatedAt,
-		UpdatedAt: userDB.UpdatedAt,
-		Email:     userDB.Email,
+	chirp := Chirp{
+		ID:        chirpDB.ID,
+		CreatedAt: chirpDB.CreatedAt,
+		UpdatedAt: chirpDB.UpdatedAt,
+		Body:      chirpDB.Body,
+		UserID:    chirpDB.UserID,
 	}
-	restpondWithJSON(w, 201, user)
-
-}
-
-func (cfg *apiConfig) resetChirpDB(w http.ResponseWriter, req *http.Request) {
-	if cfg.platform != "dev" {
-		respondWithError(w, http.StatusForbidden, "Can't remove all chirps", nil)
-	}
-	err := cfg.db.DeleteAllChirps(req.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Can't delete chirps, database error.", err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Chirps removed from database"))
+	restpondWithJSON(w, 201, chirp)
 
 }
 
@@ -85,5 +74,21 @@ func filterProfane(msg string) string {
 
 	}
 	return strings.Join(words, " ")
+
+}
+
+func (cfg *apiConfig) resetChirpDB(w http.ResponseWriter, req *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Can't remove all chirps", nil)
+	}
+	err := cfg.db.DeleteAllChirps(req.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't delete chirps, database error.", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Chirps removed from database"))
 
 }
